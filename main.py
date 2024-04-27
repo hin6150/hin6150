dcb92ddcb51e43165d513c38ddcf996363aa0f64
Shinak <hin6150@gmail.com>
2024-04-28 01:22:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,85 @@ import feedparser, time
 
 URL = "https://hin6150.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST = 5
+MAX_POST = 1
 
 markdown_text = """
+<div>
+  [![Typing SVG](https://readme-typing-svg.demolab.com?font=Indie+Flower&size=50&duration=10000&pause=2000&color=19A7CE&center=true&vCenter=true&width=450&height=100&lines=Hello+World+...+!)](https://git.io/typing-svg)
+</div>
+
+  <h3>
+    Web FrameWork
+  </h3>
+  <div>
+    <!-- https://simpleicons.org/?q=ReactNative -->
+    <!-- React -->
+    <img src="https://img.shields.io/badge/React.js-61DAFB?style=style=flat&logo=react&logoColor=black">
+    <!-- nextdotjs -->
+    <img src="https://img.shields.io/badge/Next.js-000000?style=style=flat&logo=nextdotjs&logoColor=white">
+    <!-- Vue -->
+    <img src="https://img.shields.io/badge/Vue.js-4FC08D?style=style=flat&logo=vuedotjs&logoColor=white">
+  </div>
+  
+  <h3>
+    CSS
+  </h3>
+  <div>
+    <!-- styledcomponents -->
+    <img src="https://img.shields.io/badge/Styled Components-DB7093?style=style=flat&logo=styledcomponents&logoColor=white">    
+    <!-- emotion -->
+    <img src="https://img.shields.io/badge/Emotion-D26AC2?style=style=flat&logo=styledcomponents&logoColor=white">
+    <!-- SASS -->
+    <img src="https://img.shields.io/badge/Sass-CC6699?style=style=flat&logo=sass&logoColor=white">
+    <!-- TailWind -->
+    <img src="https://img.shields.io/badge/TailWind CSS-06B6D4?style=style=flat&logo=tailwindcss&logoColor=white">
+  </div>
+  
+
+  <h3>
+    Cross-Platform
+  </h3>
+  <div>
+    <!-- ReactNative -->
+    <img src="https://img.shields.io/badge/React Native-61dafb?style=style=flat&logo=react&logoColor=black">
+    <!-- Flutter -->
+    <img src="https://img.shields.io/badge/Flutter-02569B?style=style=flat&logo=flutter&logoColor=white">
+  </div>
+  
+  <h3>
+    BackEnd & Etc..
+  </h3>
+  <div>
+    <!-- FASTAPI -->
+    <img src="https://img.shields.io/badge/FastAPI-009688?style=style=flat&logo=fastapi&logoColor=black">
+    <!-- amazonaws -->
+    <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=style=flat&logo=amazonaws&logoColor=white">
+    <!-- amazon S3 -->
+    <img src="https://img.shields.io/badge/Amazon S3-569A31?style=style=flat&logo=amazons3&logoColor=white">    
+    <!-- PostgreSQL -->
+    <img src="https://img.shields.io/badge/PostgreSQL-4169E1?style=style=flat&logo=postgresql&logoColor=white">    
+    <!-- MongoDB -->
+    <img src="https://img.shields.io/badge/MongoDB-47A248?style=style=flat&logo=mongodb&logoColor=white">
+  </div>
+
+<br />
+
+  <h3>
+    BackJoon Rank
+  </h3>
+  
+  [![Solved.ac프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=hin6150)](https://solved.ac/profile/hin6150)
+
+
+  [![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhin6150%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=dependabot.svg&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+<!--<img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=hin6150&layout=compact"> -->
+
+<!--<div align=center>
+  
+ [![GitHub Streak](https://streak-stats.demolab.com?user=hin6150&theme=highcontrast&hide_border=true&fire=EB5454&background=0D1117)](https://git.io/streak-stats)
+</div>
+-->
 ## 최근 블로그 포스팅
 
 """  # list of blog posts will be appended here
